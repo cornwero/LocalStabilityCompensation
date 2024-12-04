8bf00aaa7238392aacc4e761dd19ceef8fc3d6e8
cornwero <73069888+cornwero@users.noreply.github.com>
2024-12-04 15:16:32 -0800

    Use correct spelling for RNA types, add TwoAYGGAY to shell script.

--- data/make_compscore_histograms.sh
+++ data/make_compscore_histograms.sh
@@ -6,4 +6,5 @@ python figure_scripts/CompScoreLibraryHist.py bpRNA/bulges.txt library/libraryDa
 python figure_scripts/CompScoreLibraryHist.py bpRNA/internalloops.txt library/libraryDataInternalLoops.txt bpRNA/bpRNA_1m_90_IDs.txt
 
 python figure_scripts/CompScoreRNAType.py bpRNA/hairpins.txt bpRNA/bpRNA_1m_90_IDs.txt miRNA H1
-python figure_scripts/CompScoreRNAType.py bpRNA/hairpins.txt bpRNA/bpRNA_1m_90_IDs.txt c4\ Antisense\ RNA H1,H2
+python figure_scripts/CompScoreRNAType.py bpRNA/hairpins.txt bpRNA/bpRNA_1m_90_IDs.txt C4\ antisense\ RNA H1,H2
+python figure_scripts/CompScoreRNAType.py bpRNA/hairpins.txt bpRNA/bpRNA_1m_90_IDs.txt TwoAYGGAY\ RNA H1,H2
