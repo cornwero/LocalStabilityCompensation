7959aa71997168e2936274f08b40934fea3008a2
cornwero <73069888+cornwero@users.noreply.github.com>
2025-04-03 17:47:51 -0700

    correct refold2 -> refold

--- data/make_phylogenetic_histograms.sh
+++ data/make_phylogenetic_histograms.sh
@@ -1,6 +1,6 @@
 python figure_scripts/CompScorePhylo.py bpRNA/hairpins.txt miRNA Viridiplantae,Metazoa
 python figure_scripts/CompScorePhylo.py bpRNA/bulges.txt miRNA Viridiplantae,Metazoa
 python figure_scripts/CompScorePhylo.py bpRNA/internalloops.txt miRNA Viridiplantae,Metazoa
-python figure_scripts/CompScorePhylo.py bpRNA/refold2/hairpins.txt miRNA Viridiplantae,Metazoa
-python figure_scripts/CompScorePhylo.py bpRNA/refold2/bulges.txt miRNA Viridiplantae,Metazoa
-python figure_scripts/CompScorePhylo.py bpRNA/refold2/internalloops.txt miRNA Viridiplantae,Metazoa
+python figure_scripts/CompScorePhylo.py bpRNA/refold/hairpins.txt miRNA Viridiplantae,Metazoa
+python figure_scripts/CompScorePhylo.py bpRNA/refold/bulges.txt miRNA Viridiplantae,Metazoa
+python figure_scripts/CompScorePhylo.py bpRNA/refold/internalloops.txt miRNA Viridiplantae,Metazoa
--- data/make_stemloop_energy_scatterplots.sh
+++ data/make_stemloop_energy_scatterplots.sh
@@ -1,9 +1,9 @@
 mkdir -p figures
-python figure_scripts/stem_scatter.py bpRNA/bpRNA_1m_90_IDs.txt bpRNA/refold2/hairpins.txt 0.2
-python figure_scripts/stem_scatter.py bpRNA/bpRNA_1m_90_IDs.txt bpRNA/refold2/bulges.txt 0.2
-python figure_scripts/stem_scatter.py bpRNA/bpRNA_1m_90_IDs.txt bpRNA/refold2/bulges.txt 0.2 no1nt
-python figure_scripts/stem_scatter.py bpRNA/bpRNA_1m_90_IDs.txt bpRNA/refold2/internalloops.txt 0.2
-python figure_scripts/stem_scatter.py False bpRNA/refold2/hairpins.txt 0.2
-python figure_scripts/stem_scatter.py False bpRNA/refold2/bulges.txt 0.2
-python figure_scripts/stem_scatter.py False bpRNA/refold2/bulges.txt 0.2 no1nt
-python figure_scripts/stem_scatter.py False bpRNA/refold2/internalloops.txt 0.2
+python figure_scripts/stem_scatter.py bpRNA/bpRNA_1m_90_IDs.txt bpRNA/refold/hairpins.txt 0.2
+python figure_scripts/stem_scatter.py bpRNA/bpRNA_1m_90_IDs.txt bpRNA/refold/bulges.txt 0.2
+python figure_scripts/stem_scatter.py bpRNA/bpRNA_1m_90_IDs.txt bpRNA/refold/bulges.txt 0.2 no1nt
+python figure_scripts/stem_scatter.py bpRNA/bpRNA_1m_90_IDs.txt bpRNA/refold/internalloops.txt 0.2
+python figure_scripts/stem_scatter.py False bpRNA/refold/hairpins.txt 0.2
+python figure_scripts/stem_scatter.py False bpRNA/refold/bulges.txt 0.2
+python figure_scripts/stem_scatter.py False bpRNA/refold/bulges.txt 0.2 no1nt
+python figure_scripts/stem_scatter.py False bpRNA/refold/internalloops.txt 0.2
